a5fc496a39dc7f61656ea1e3fdd90d30c32c8e81
Kate-bee <kate280987@gmail.com>
2021-08-18 10:20:30 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array && array.length) {
+    let min = array[0];
+    for (i = 1; i < array.length; i++) {
+      if (array[i] < min) min = array[i];
+    }
+    return min;
+  }
+  else return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array && array.length) {
+    let max = array[0];
+    for (i = 1; i < array.length; i++) {
+      if (array[i] > max) max = array[i];
+    }
+    return max;
+  }
+  else return 0;
+}
+exports.avg = function avg(array) {
+  if (array && array.length) {
+
+    let sum = 0;
+    for (i = 0; i < array.length; i++) {
+      sum = sum + array[i];
+    }
+    let av = sum / array.length;
+    return av;
+  }
+  else return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
-}
